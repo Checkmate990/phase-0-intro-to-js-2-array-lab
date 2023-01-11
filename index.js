3577e23505096835fdfeb7f8f6327df9f7db2451
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-01-11 11:31:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo","Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield')
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo')
+}
+
+function appendCat(){
+    const broom = [...cats, 'Broom']
+    return broom
+}
+
+function prependCat(){
+    const arnold = ['Arnold', ...cats]
+    return arnold
+}
+
+function removeLastCat(){
+   const last =  [...cats]
+   last.pop()
+   return last
+}
+
+function removeFirstCat(){
+    const first =  [...cats]
+    first.shift()
+    return first
+}
